cba3d0a859b7a00c3b7b0448d849fbf622a8b04b
DiSantoz <dferreira0191@gmail.com>
2023-08-05 17:47:51 -0400

    display captured user prompts on html

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-//const fs = require("fs");
-//const generatePage = require("./src/page-template");
+const fs = require("fs");
 const inquirer = require("inquirer");
-
-// const pageHTML = generatePage(name, github);
-
-// // use Node module to write File, first create file, then contents, then error
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! View index.html");
-// });
+const generatePage = require("./src/page-template");
 
 const promptUser = () => {
-  console.log(`
-  =================
-  Welcome to nodePortfolio!
-  =================
-  `);
   return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
     },
     {
       type: "input",
       name: "about",
       message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => confirmAbout,
     },
   ]);
 };
@@ -42,6 +52,8 @@ const promptProject = (portfolioData) => {
 Add a New Project
 =================
 `);
+
+  // If there's no 'projects' array property, create one
   if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
@@ -50,17 +62,33 @@ Add a New Project
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project name!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project description!");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
         name: "languages",
-        message: "What did you build this project with? (Check all that apply)",
+        message: "What did you this project with? (Check all that apply)",
         choices: [
           "JavaScript",
           "HTML",
@@ -75,6 +103,14 @@ Add a New Project
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project GitHub link!");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
@@ -102,5 +138,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
